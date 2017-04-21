b32d2d97ef66a8005f9c9b8fb5d005623a50f228
BidziS <danielcudnik93@gmail.com>
2017-04-21 12:30:39 +0200

    Configure IoC container (Autofac), add autofac, automapper to bll and entity framework to dal

---
+++ MakeComputerShop.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using MakeComputerShop.Bll;
+
+namespace MakeComputerShop.Web.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void ConfigurationAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(WebApiApplication).Assembly);
+
+            builder.RegisterModule(new ServicesModule());
+
+            builder.RegisterControllers(typeof(WebApiApplication).Assembly);
+
+            var container = builder.Build();
+
+
+            // Set the dependency resolver for MVC.
+            var mvcResolver = new AutofacDependencyResolver(container);
+            DependencyResolver.SetResolver(mvcResolver);
+        }
+    }
+}
