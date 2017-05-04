c35ca4f02f61d188e27fd44b4dab761053c471f7
Radosław Cybulski <radek199317@wp.pl>
2017-05-04 21:09:33 +0200

    Add models GraphicCard, Drive, NetworkCard and SoundCard and repositors

---
+++ MakeComputerShop.Dal/Models/GraphicsCardDb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeComputerShop.Dal.Models
+{
+    public class GraphicsCardDb:BaseDb
+    {
+        public ProducentDb Producent { get; set; }
+
+        [Required]
+        public int ProducentId { get; set; }
+
+        [Required]
+        public string ChipsetName { get; set; }
+        [Required]
+        public int ChipsetId { get; set; }
+
+        [Required]
+        public int MemorySize { get; set; }
+
+        [Required]
+        public int DataBusBit { get; set; }
+
+        [Required]
+        public string Connector { get; set; }
+
+    }
+}
---
+++ MakeComputerShop.Dal/Models/SoundCardDb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeComputerShop.Dal.Models
+{
+    public class SoundCardDb:BaseDb
+    {
+        public ProducentDb Producent { get; set; }
+
+        
+
+        [Required]
+        public int ProducentId { get; set; }
+
+        [Required]
+        public string Interface { get; set; }
+
+        [Required]
+        public float SoundSystem { get; set; }
+
+
+        
+
+
+
+      
+    }
+}
